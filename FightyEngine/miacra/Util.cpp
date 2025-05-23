7c8cf95700512f04adea4cb4fd67c3c6e34e2516
NEWSKINNE® <innoforker@proton.me>
2025-05-23 21:38:23 +0300

    Update Util.cpp

--- FightyEngine/miacra/Util.cpp
+++ FightyEngine/miacra/Util.cpp
@@ -2,7 +2,7 @@
 
 namespace Util
 {
-    std::string GetCurrentTime(std::string format = "%Y-%m-%d %H:%M:%S")
+    std::string GetCurrentTime(std::string format)
     {
         auto now = std::chrono::system_clock::now();
         std::time_t now_c = std::chrono::system_clock::to_time_t(now);
